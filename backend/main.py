ba6cd3df993382feb690c47d2535bc4b616a596d
Arowe14 <alexrowe0425@outlook.com>
2026-01-29 11:59:44 -0500

    Update main.py with netlify

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,11 @@ app = FastAPI()
 # In dev, allow React dev server. In production, set this to your real domain.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://image-converter-pdf-merge.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
